66d0651671995b28e8a34cb17ab0672b8f90fc3b
Ahmed Mustafa Malik <ahmedmustafamalik98@gmail.com>
2024-06-19 23:56:07 +0200

    leftover-apples

--- assets/demos.js
+++ assets/demos.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var typed = new Typed('#typed', {
-    stringsElement: '#typed-strings',
-    typeSpeed: 20,
-    backSpeed: 20,
-    startDelay: 100,
+  new Typed('#typed', {
+    strings: ['&#x0041; &#x1F44B;'],
+    // stringsElement: '#typed-strings',
+    typeSpeed: 1000,
+    backSpeed: 1000,
     loop: true,
     loopCount: Infinity,
     onBegin: function (self) {
@@ -39,84 +39,6 @@ document.addEventListener('DOMContentLoaded', function () {
     onDestroy: function (self) {
       prettyLog('onDestroy ' + self);
     },
-  });
-
-  document.querySelector('.toggle').addEventListener('click', function () {
-    typed.toggle();
-  });
-  document.querySelector('.stop').addEventListener('click', function () {
-    typed.stop();
-  });
-  document.querySelector('.start').addEventListener('click', function () {
-    typed.start();
-  });
-  document.querySelector('.reset').addEventListener('click', function () {
-    typed.reset();
-  });
-  document.querySelector('.destroy').addEventListener('click', function () {
-    typed.destroy();
-  });
-  document.querySelector('.loop').addEventListener('click', function () {
-    toggleLoop(typed);
-  });
-
-  var typed2 = new Typed('#typed2', {
-    strings: [
-      'Some <i>strings</i> with',
-      'Some <strong>HTML</strong>',
-      'Chars &times; &copy;',
-    ],
-    typeSpeed: 0,
-    backSpeed: 0,
-    fadeOut: true,
-    loop: true,
-  });
-  document.querySelector('.loop2').addEventListener('click', function () {
-    toggleLoop(typed2);
-  });
-
-  new Typed('#typed3', {
-    strings: [
-      'My strings are: <i>strings</i> with',
-      'My strings are: <strong>HTML</strong>',
-      'My strings are: Chars &times; &copy;',
-    ],
-    typeSpeed: 0,
-    backSpeed: 0,
-    smartBackspace: true,
-    loop: true,
-  });
-
-  new Typed('#typed4', {
-    strings: ['Some strings without', 'Some HTML', 'Chars'],
-    typeSpeed: 0,
-    backSpeed: 0,
-    attr: 'placeholder',
-    bindInputFocusEvents: true,
-    loop: true,
-  });
-
-  new Typed('#typed5', {
-    strings: [
-      '1 Some <i>strings</i> with',
-      '2 Some <strong>HTML</strong>',
-      '3 Chars &times; &copy;',
-    ],
-    typeSpeed: 0,
-    backSpeed: 0,
-    shuffle: true,
-    cursorChar: '_',
-    smartBackspace: false,
-    loop: true,
-  });
-
-  new Typed('#typed6', {
-    strings: [
-      'npm install^1000\n`installing components...` ^1000\n`Fetching from source...`',
-    ],
-    typeSpeed: 40,
-    backSpeed: 0,
-    loop: true,
   });
 });
 
